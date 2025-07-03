19ab49dad530faf36deb449e74e15fb43df9e768
splguyjr <splguyjr@naver.com>
2025-07-03 20:17:14 +0900

    [init] 프로젝트 생성 및 초기 설정

---
+++ src/test/java/com/dao/momentum/MomentumDaoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dao.momentum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentumDaoBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
